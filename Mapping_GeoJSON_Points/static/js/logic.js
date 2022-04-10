16be0beea5b4a504717dd7b487aca69fae998061
Hristo Bustamante <hristobustamante@Hristos-MBP.fios-router.home>
2022-04-10 17:13:28 -0400

    adding this code

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,8 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -12,18 +10,31 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-str
   zoomOffset: -1,
   accessToken: API_KEY
 });
-// Add GeoJSON data.
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/majorAirports.json";
-// Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
 });
-
-
-
-
-
-
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+// Pass our amp layers into our layer control and add the layer control to the map
+L.control.layers(baseMaps).addTo(map);
+// Acessing to the airport GeoJSON URL
+let airportData = "https://raw.githubusercontent.com/hbustamante8/Mapping_Earthquakes/master/majorAirports.json";
+// Grabbing our GeoJSON data.
+d3.json(airportData).then(function(data){
+  console.log(data);
+  // Creating a GeoJSON layer with retrieved data
+  L.geoJson(data).addTo(map);
+  
+})
